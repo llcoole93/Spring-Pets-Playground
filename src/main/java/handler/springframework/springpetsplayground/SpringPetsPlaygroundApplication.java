08b5836c372d70aab6b3dc79a56882b745145fa9
Mortimus <llcoole93@gmail.com>
2021-03-30 20:06:33 -0400

    init

---
+++ src/main/java/handler/springframework/springpetsplayground/SpringPetsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package handler.springframework.springpetsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/handler/springframework/springpetsplayground/SpringPetsPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package handler.springframework.springpetsplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetsPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
