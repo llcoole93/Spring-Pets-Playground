ef182978390b1fea83db173946585cd6dcafdf0a
Mortimus <llcoole93@gmail.com>
2021-03-31 20:33:59 -0400

    establishing multimodule project

--- spring-pets-playground-web/src/test/java/springpetsplayground/SpringPetsPlaygroundApplicationTests.java
+++ spring-pets-playground-web/src/test/java/springpetsplayground/SpringPetsPlaygroundApplicationTests.java
@@ -1,4 +1,4 @@
-package handler.springframework.springpetsplayground;
+package springpetsplayground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
