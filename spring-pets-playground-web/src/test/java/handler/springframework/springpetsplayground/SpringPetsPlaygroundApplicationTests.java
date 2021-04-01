de92353054dad1d71d6c344f3cc7f5b6a4c5bb1b
Mortimus <llcoole93@gmail.com>
2021-03-31 22:19:40 -0400

    fix tests

--- spring-pets-playground-web/src/test/java/handler/springframework/springpetsplayground/SpringPetsPlaygroundApplicationTests.java
+++ spring-pets-playground-web/src/test/java/handler/springframework/springpetsplayground/SpringPetsPlaygroundApplicationTests.java
@@ -1,4 +1,4 @@
-package springpetsplayground;
+package handler.springframework.springpetsplayground;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
