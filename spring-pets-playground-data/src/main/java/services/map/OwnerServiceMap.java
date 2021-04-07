6a93b0dbc14555d31f743b07c899d593c2b2cda6
Mortimus <llcoole93@gmail.com>
2021-04-07 19:11:53 -0400

    implement abstract classes closes #6

---
+++ spring-pets-playground-data/src/main/java/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package services.map;
+
+import handler.springframework.springpetsplayground.model.Owner;
+import services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by Evan on 4/7/2021
+ */
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
