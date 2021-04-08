ea83ec3a0c93cb6c249c7d90f75c39e97e4f965c
Mortimus <llcoole93@gmail.com>
2021-04-07 21:14:59 -0400

    #7 added remap for owners

--- spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/controllers/OwnersController.java
+++ spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/controllers/OwnersController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Evan on 4/7/2021
  */
 @Controller
-public class OwnerController {
-    @RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
+public class OwnersController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
     public String listowner() {
-        return "owner/index";
+        return "owners/index";
     }
 }
