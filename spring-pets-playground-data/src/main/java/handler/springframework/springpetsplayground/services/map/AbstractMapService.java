7b76603a267b30ecd6cc2a177ab55884a468b0b5
Mortimus <llcoole93@gmail.com>
2021-04-09 00:29:40 -0400

    She pretty with the IDs now #21

--- spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/map/AbstractMapService.java
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/map/AbstractMapService.java
@@ -1,16 +1,15 @@
 package handler.springframework.springpetsplayground.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import handler.springframework.springpetsplayground.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * Created by Evan on 4/7/2021
  */
-public abstract class AbstractMapService <T, ID> {
+public abstract class AbstractMapService <T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -20,8 +19,17 @@ public abstract class AbstractMapService <T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T obj) {
-        map.put(id, obj);
+    T save(T obj) {
+
+        if (obj != null) {
+            if (obj.getId() == null) {
+                obj.setId(getNextId());
+            }
+
+            map.put(obj.getId(), obj);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return obj;
     }
@@ -32,5 +40,15 @@ public abstract class AbstractMapService <T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/bootstrap/DataInitializer.java
+++ spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/bootstrap/DataInitializer.java
@@ -24,21 +24,18 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Evan");
         owner2.setLastName("Michale");
         
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("simpsons");
         owner3.setLastName("dev");
         
@@ -47,21 +44,18 @@ public class DataInitializer implements CommandLineRunner {
         System.out.println("Loaded owners......");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Prince");
         vet1.setLastName("Goopy");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Danny");
         vet2.setLastName("Devito");
 
         vetService.save(vet2);
 
         Vet vet3 = new Vet();
-        vet3.setId(3L);
         vet3.setFirstName("Ron");
         vet3.setLastName("Johnson");
 
