824984a41e76fa1e60d98185468b0b155ed06e0e
Mortimus <llcoole93@gmail.com>
2021-04-09 00:13:27 -0400

    vets in the house closes #15

--- spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/controllers/VetsController.java
+++ spring-pets-playground-web/src/main/java/handler/springframework/springpetsplayground/controllers/VetsController.java
@@ -1,6 +1,8 @@
 package handler.springframework.springpetsplayground.controllers;
 
+import handler.springframework.springpetsplayground.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
