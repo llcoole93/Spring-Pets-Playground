cb783701392b1e4ca8b201f2d7f83e2aedbb6485
Mortimus <llcoole93@gmail.com>
2021-11-01 20:04:18 -0400

    created visit entity closes #26

--- spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/model/Owner.java
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/model/Owner.java
@@ -1,7 +1,11 @@
 package handler.springframework.springpetsplayground.model;
 
+import java.util.Set;
+
 /**
  * Created by Evan on 03 / 2021
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 }
