85e6fc9a9865c1410e067d6b743a540532e65d35
Mortimus <llcoole93@gmail.com>
2021-11-01 20:37:03 -0400

    Implement petType service closes #28

---
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package handler.springframework.springpetsplayground.services;
+
+import handler.springframework.springpetsplayground.model.PetType;
+
+/**
+ * Created by Evan on 04 / 2021
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/map/PetTypeMapService.java
@@ -0,0 +1,37 @@
+package handler.springframework.springpetsplayground.services.map;
+
+import handler.springframework.springpetsplayground.model.PetType;
+import handler.springframework.springpetsplayground.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * Created by Evan on 11/1/2021
+ */
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void delete(PetType object) {
+
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return null;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
