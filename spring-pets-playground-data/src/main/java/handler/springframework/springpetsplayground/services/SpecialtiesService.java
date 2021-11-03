f61837f71d6713335fa768eb69b829bc81eac5b3
Mortimus <llcoole93@gmail.com>
2021-11-02 22:46:55 -0400

    create specialty map service closes #32

---
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/SpecialtiesService.java
@@ -0,0 +1,10 @@
+package handler.springframework.springpetsplayground.services;
+
+import handler.springframework.springpetsplayground.model.Specialty;
+
+/**
+ * @author Evan on 11/2/2021
+ */
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
---
+++ spring-pets-playground-data/src/main/java/handler/springframework/springpetsplayground/services/map/SpecialtiesMapService.java
@@ -0,0 +1,39 @@
+package handler.springframework.springpetsplayground.services.map;
+
+import handler.springframework.springpetsplayground.model.Specialty;
+import handler.springframework.springpetsplayground.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author Evan on 11/2/2021
+ */
+@Service
+public class SpecialtiesMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
